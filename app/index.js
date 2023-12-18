463ecc976ce65f6a1f3b714c87198d9a85f040aa
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2023-12-18 20:23:35 +0530

    Refactor layout and add new screens

--- app/index.js
+++ app/index.js
@@ -1,19 +1,46 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text,useState, ScrollView, SafeAreaView } from 'react-native'
 import React, { useEffect } from 'react'
 import { useNavigation } from 'expo-router'
+import {Welcome, Nearbyjobs, Popularjobs} from '../components/'
+import { useRouter, Stack } from 'expo-router'
+import { COLORS, icons, images, SIZES } from '../constants'
+
 
 const index = () => {
     const navigation = useNavigation()
+    const router = useRouter()
 
-    useEffect(() => {
-        navigation.setOptions({
-            headerTitle: 'JobsFinder',
-        })
-    }, [])
+
   return (
-    <ScrollView>
-      <Text>index</Text>
-    </ScrollView>
+
+      <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+        <Stack.Screen
+        options={{
+          headerShown: true,
+          title: 'JobsFinder',
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerTintColor: COLORS.primary,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerLargeTitle: true,
+          headerBlurEffect: 'systemUltraThinMaterialDark',	
+          headerTransparent: true,
+          headerLargeTitleShadowVisible: false,
+
+
+
+        }}
+        >
+
+      <Welcome />
+      <Nearbyjobs />
+      <Popularjobs />
+        </Stack.Screen>
+      </SafeAreaView>
+   
   )
 }
 
